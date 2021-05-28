f33d8f3ebd0fb30d0e2438ecd3d716333e9370b5
m250888 <ths0221>
2021-05-27 23:07:45 -0300

    formularios,mocks e assercoes

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,21 @@
+///   <reference types="cypress"/>
+
+
+
+context('Cadastro', () => {
+    it('cadastro de usuario no site', () => {
+        cy.visit('Register.html')
+
+
+      cy.get('input[placeholder="First Name"]').type('Aluno')
+      cy.get('input[placeholder="Last Name"]').type('Agilizei')
+      cy.get('input[ng-model="EmailAdress"]').type('aluno@email.com')
+      cy.get('input[placeholder="Phone"]').type('123456789')
+      
+
+
+
+
+
+    });
+});
